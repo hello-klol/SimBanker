a5c40a2cbb36ea59cc62aa1367e433512e3366f0
katie.clark <katie.clark@icrossing.co.uk>
2013-09-17 21:27:24 +0100

    average house price things

--- app/scripts/views/HousePriceView.js
+++ app/scripts/views/HousePriceView.js
@@ -22,6 +22,11 @@ define([
         updatePrice: function(){
             this.age++;
             var nextPrice = this.prices[this.age % this.prices.length];
+            console.log(nextPrice['change'].toString()[0]);
+            nextPrice['price'] = nextPrice['price'].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");;
+            if(nextPrice['change'].toString()[0]!='-') {
+                nextPrice['change'] ='+'+nextPrice['change'];
+            }
             var housepriceitem = houseprice(nextPrice);
             housepriceitem.remove();
             this.$el.html(housepriceitem);
